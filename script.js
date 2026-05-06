0d4278c0bcae5476ae340cc6e651decd92d02238
tarakanovandrej1635 <tarakanovandrej1635@gmail.com>
2026-05-06 10:42:33 +0300

    Добавил Readme

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 const menu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile__menu')
 
-menu.addEventListener('click',()=>{
+menu.addEventListener('click', ()=> {
     mobileMenu.classList.toggle('mobile__menu-active')
     if (mobileMenu.classList.contains('mobile__menu-active')) {
         document.body.style.overflow = 'hidden'
-    }else{
+    } else{
         document.body.style.overflow = ''
     }
-    
 })
-
-
-
-
